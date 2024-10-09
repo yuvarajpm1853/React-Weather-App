9ea01f670e5fa0c79df0c6d0538af8e42d293c31
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-09 15:09:59 +0530

    Added Weather Comp & useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-
-// https://github.com/Makin-Things/weather-icons?tab=readme-ov-file
-import clearDayIcon from './assets/clear-day.svg'
-import clearNightIcon from './assets/clear-night.svg'
-import cloudyDayIcon from './assets/cloudy-3-day.svg'
-import cloudyNightIcon from './assets/cloudy-3-night.svg'
-import cloudyIcon from './assets/cloudy.svg'
-import ShowerRainIcon from './assets/hail.svg'
-import rainDayIcon from './assets/rainy-3-day.svg'
-import rainNightIcon from './assets/rainy-3-night.svg'
-import thunderstormIcon from './assets/scattered-thunderstorms.svg'
-import snowIcon from './assets/snowy-3.svg'
-import mistIcon from './assets/fog.svg'
-import windIcon from './assets/wind.svg'
-import humidityIcon from './assets/humidity.svg'
 import searchIcon from './assets/search.png'
+import WeatherDetails from './WeatherDetails';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("Chennai");
+
 
   return (
     <>
 <div className="container">
   <div className="input-container">  
   <input type="text" className="cityInput" 
-  placeholder="Search city" />
+  placeholder="Search city" value={city} onChange={e=>setCity(e.target.value)}/>
   <div className="search-icon">
     {/* https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2 */}
     <img src={searchIcon} alt="Search" />
     </div>
   </div>
+  <WeatherDetails city={city} />
   </div>
      </>
   )
---
+++ src/WeatherDetails.jsx
@@ -0,0 +1,51 @@
+// https://github.com/Makin-Things/weather-icons?tab=readme-ov-file
+import clearDayIcon from './assets/clear-day.svg'
+import clearNightIcon from './assets/clear-night.svg'
+import cloudyDayIcon from './assets/cloudy-3-day.svg'
+import cloudyNightIcon from './assets/cloudy-3-night.svg'
+import cloudyIcon from './assets/cloudy.svg'
+import ShowerRainIcon from './assets/hail.svg'
+import rainDayIcon from './assets/rainy-3-day.svg'
+import rainNightIcon from './assets/rainy-3-night.svg'
+import thunderstormIcon from './assets/scattered-thunderstorms.svg'
+import snowIcon from './assets/snowy-3.svg'
+import mistIcon from './assets/fog.svg'
+import windIcon from './assets/wind.svg'
+import humidityIcon from './assets/humidity.svg'
+import { useState } from 'react'
+
+const WeatherDetails = ({city}) => {
+
+    const [icon, setIcon] = useState(thunderstormIcon)
+    const [temp, setTemp] = useState(0);
+    const [lat, setLat] = useState(0);
+    const [long, setLong] = useState(0);
+    const [country, setCountry] = useState("IN");
+    const [humidity, setHumidity] = useState();
+    const [windSpeed, setWindSpeed] = useState();
+
+    return ( 
+        <>
+        <div className="image">
+            <img src={icon} alt="Weather Image" />
+        </div>
+        <div className="temp">{temp}</div>
+        <div className="city">{city}</div>
+        <div className="country">{country}</div>
+        <div className="temp">{temp}</div>
+        <div className="cord">
+            <div>
+                <span className="lat">latitude</span>
+                <span>{lat}</span>
+            </div>
+            <div>
+                <span className="long">longitude</span>
+                <span>{long}</span>
+            </div>
+        </div>
+
+        </>
+     );
+}
+ 
+export default WeatherDetails;
